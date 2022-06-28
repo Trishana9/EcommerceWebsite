48cd88699a6d744c6d0da72c9bef401ff4ad7a65
Trishana9 <trishana.rai@introcept.co>
2022-06-28 18:06:10 +0545

    Product listing

--- src/components/ProductListing.vue
+++ src/components/ProductListing.vue
@@ -17,6 +17,48 @@
             </div>
         </div>
 	</nav>
+    <main>
+		<div class="mt-28 mx-36 sm:mx-10">
+            <div class="productlist grid grid-cols-4 gap-5 sm:grid-cols-2 " id="productlist">
+                <div v-for="product in productList" :key="product">
+                    <div class="border flex flex-col justify-center py-8 cursor-pointer">
+                        <div class="flex justify-center items-center">
+                            <img :src="product.thumbnail" class="h-64 w-80">
+                        </div>
+                        <div class="px-10">
+                            <p>{{product.title}}</p>
+                            <p class="font-bold text-emerald-500">Rs.{{(product.price-((product.price*product.discountPercentage)/100)).toFixed(2)}}</p>
+                            <div class="flex">
+                                <p class="text-gray-400 mr-3 line-through text-sm">{{product.price}}</p>
+                                <p class="text-sm">-{{product.discountPercentage}}%</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</main>
+    
 </section>
     
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: "ProductCard",
+  data()
+  {
+      return{
+          productList:[]
+      }
+  },
+  async mounted()
+  {
+      let res=await axios.get('https://dummyjson.com/products');
+      console.log(res)
+      this.productList=res.data.products
+      console.log(this.productList)
+  }
+};
+</script>
+
